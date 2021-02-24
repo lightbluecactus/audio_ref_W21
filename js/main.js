41a02a13a25cde8fe6ae0d0acae61ca4d122481e
lightbluecactus <violetday77@yahoo.com>
2021-02-24 14:24:25 -0400

    added dynamic audio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-console.log("javascript is linked up");
+// create a module for our project
+(() => {
+  console.log("javascript is linked up");
+
+  let theAudio = document.querySelector("audio"),
+      playButton = document.querySelector("button"),
+      albumArt = document.querySelectorAll(".track-ref");
+
+  function playTrack(){
+    // grab the value of the custom data attribute
+    let targetTrack = this.dataset.trackref;
+    // load the new audio source
+    theAudio.src = `audio/${targetTrack}.mp3`;
+
+    theAudio.volume = 0.5; //50%
+    // load and play the new source
+    theAudio.load();
+    theAudio.play();
+  }
+
+  for (cover of albumArt){
+    debugger;
+    cover.addEventListener("click", playTrack);
+  }
+
+})()
